8e4b1c1b7d677c174368eb6e3f779b4ea0aecea7
Suyoscar11 <suyoscar11@gmail.com>
2024-09-21 01:38:06 -0600

    completed 2 routes: RegisterUser and LoginUser

--- Professor_Api/index.js
+++ Professor_Api/index.js
@@ -8,6 +8,44 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.post("/api/user/registerUser", async function (req, res) {
+  const { name, pass } = req.body;
+  try {
+    if (!name || !pass) {
+      return res.status(400).json({ msg: "Fields cannot be empty." });
+    }
+    let User = await user.findOne({ name, pass });
+    if (User) {
+      return res.status(400).json({ msg: "User aldready exists" });
+    }
+    User = new user({
+      name,
+      pass,
+    });
+    await User.save();
+    res.status(200).json({ msg: "Success" });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+app.post("/api/user/loginUser", async function (req, res) {
+  const { name, pass } = req.body;
+
+  try {
+    if (!name || !pass) {
+      return res.status(400).json({ msg: "Fields cannot be Empty" });
+    }
+    let User = await user.findOne({ name, pass });
+    if (!User) {
+      return res.status(400).json({ msg: "Invalid Credentials!" });
+    }
+
+    res.status(200).json({ msg: "Success", userid: User._id });
+  } catch (error) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 app.get("/", async function (req, res) {
   res.status(500).send("Hello World!");
 });
