8c26533ab521dde717d161064ee6c898625d9b28
Suyoscar11 <suyoscar11@gmail.com>
2024-09-22 14:04:18 -0600

    completed Fetch appointment by Student Id and status "booked"

--- Professor_Api/index.js
+++ Professor_Api/index.js
@@ -50,6 +50,47 @@ app.get("/", async function (req, res) {
   res.status(500).send("Hello World!");
 });
 
+// Fetch professors by category
+
+app.get("/api/user/getProf_byCategory/:category", async function (req, res) {
+  const { category } = req.params;
+
+  try {
+    if (!category) {
+      return res.status(400).json({ msg: "All Fields required" });
+    }
+    const professors = await professor.find({ speciality: category });
+    if (professors.length === 0) {
+      return res
+        .status(400)
+        .json({ msg: "No professors found of this category" });
+    }
+    res.status(200).json({ msg: professors });
+  } catch (err) {
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+// Fetch appointment by student ID and status "booked"
+app.get(
+  "/api/user/getAppointments_byStudentId/:std_id",
+  async function (req, res) {
+    const { std_id } = req.params;
+
+    try {
+      if (!std_id) {
+        return res.status(400).json({ msg: "Please fill all the fields" });
+      }
+
+      const appointments = await appointment.find({ std_id, status: "booked" });
+      if (appointments.length === 0) {
+        return res.status(200).json({ msg: appointments });
+      }
+    } catch (error) {
+      res.status(500).json({ msg: "Server Error" });
+    }
+  }
+);
+
 const port = 5600;
 app.listen(port, function () {
   console.log("Server is running in port: ", port);
